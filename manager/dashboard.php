f2dc64b1ef1339ad0578f3463cf232b8d4eb7618
Kreety7 <awalkreety@gmail.com>
2024-10-23 15:03:08 +0545

    sidebar

--- manager/dashboard.php
+++ manager/dashboard.php
@@ -1,40 +1,44 @@
 <?php
-require_once "../logincheck.php";
+// require_once "../logincheck.php";
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <title>Employee Dashboard</title>
-    <link rel="stylesheet" href="styles.css">
+    <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=dashboard" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=person" /> -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=pending_actions" />   
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=edit_calendar" />     
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> 
 </head>
 <body>
-    <div class="container">
-        <aside>
-            <div class="top">
-                
-            <div class="logo">
-             <h2>C<span class="danger">BABAR</span></h2>
-</div>
-<div class="close">
-    <span class="material-symbols-sharp">close</span>
-</div>
-</div>
+<div class="container">
+<aside>
 <div class="sidebar">
-    <a href="#">
-        <span class="material-symbols-sharp">grid_view</span>
-        <h3>Dashboard</h3>
+<a href="#">
+    <span class="material-symbols-outlined">
+dashboard
+</span>
+<h3>Dashboard</h3>
+
 </a>
 <a href="#" class="active">
-    <span class="material-symbols-sharp">person_outline</span>
+<span class="material-symbols-outlined">
+person 
+</span>
     <h3>Profile</h3>
 </a>
 <a href="#">
-<span class="material-symbols-sharp">pen-to-square</span>
+<span class="material-symbols-outlined">
+pending_actions
+</span>
 <h3>Leave Request</h3>
 </a>
 <a href="#">
-<span class="material-symbols-sharp">receipt_long</span>
+<span class="material-symbols-outlined">
+edit_calendar
+</span>
 <h3>Leave Subsitution</h3>
 </a>
 
